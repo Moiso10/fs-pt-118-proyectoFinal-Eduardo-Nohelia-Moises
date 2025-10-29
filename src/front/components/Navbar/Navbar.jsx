d1540ede3be14fb2af19941ac24d73135dbe1131
noheliabam <noheliabam1980@gmail.com>
2025-10-29 22:05:54 +0100

    Restaurado Navbar desde develop)

--- src/front/components/Navbar/Navbar.jsx
+++ src/front/components/Navbar/Navbar.jsx
@@ -1,81 +1,79 @@
-import React, { useContext } from "react";
-import { Link, useNavigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import movieVerseLogo from "../../assets/img/MovieVerse.png";
-import { Context } from "../../appContext";
 import "./Navbar.css";
 import useGlobalReducer from "../../hooks/useGlobalReducer";
 
 
 export const Navbar = () => {
 
-  const { store, dispatch } = useGlobalReducer();
-  const navigate = useNavigate()
+    const { store, dispatch } = useGlobalReducer();
+	const navigate = useNavigate()
 
-  const handleLogout = () => {
+	const handleLogout = () => {
+		
+		dispatch({ type: "user_logged_out" });
+		
+		navigate("/");
+	};
 
-    dispatch({ type: "user_logged_out" });
 
-    navigate("/");
-  };
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <Link to="/" className="navbar-brand mb-0">
+                    <img src={movieVerseLogo} alt="MovieVerse" />
+                </Link>
 
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link to="/" className="navbar-brand mb-0">
-          <img src={movieVerseLogo} alt="MovieVerse" />
-        </Link>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Inicio</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/peliculas" className="nav-link">Películas</Link>
-            </li>
-
-            {store.auth ? (
-              // Si está logueado
-              <>
-                <li className="nav-item">
-                  <Link to="/profile" className="nav-link">Perfil</Link>
-                </li>
                 <button
-                  className="btn btn-danger"
-                  onClick={handleLogout}
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
                 >
-                  Cerrar sesión
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-              </>
-            ) : (
-              // Si NO está logueado
-              <>
-                <li className="nav-item">
-                  <Link to="/register" className="nav-link">Register</Link>
-                </li>
-                <li className="nav-tiem">
-                  <Link to="/login" className="nav-link">Login</Link>
-                </li>
-              </>
-            )}
 
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Inicio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/peliculas" className="nav-link">Películas</Link>
+                        </li>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+                        {store.auth ? (
+					// Si está logueado
+					<>
+						<li className="nav-item">
+							<Link to="/profile" className="nav-link">Perfil</Link>
+						</li>
+						<button 
+							className="btn btn-danger"
+							onClick={handleLogout}
+						>
+							Cerrar sesión
+						</button>
+					</>
+				) : (
+					// Si NO está logueado
+					<>
+						<li className="nav-item">
+							<Link to="/register" className="nav-link">Register</Link>
+						</li>
+						<li className="nav-tiem">
+							<Link to ="/login" className="nav-link">Login</Link>
+						</li>
+					</>
+				)}                    
+                       
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
