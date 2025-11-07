e4dd371891ee7549fd5099f0375f722e7bd960e0
Edusanba <edu.santiagoba@hotmail.com>
2025-11-07 18:32:57 +0100

    Completando css

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -7,6 +7,7 @@ import PageTransition from "../components/Transition/PageTransition";
 export const Layout = () => {
   const location = useLocation();
   const [entering, setEntering] = useState(true);
+  const isLanding = location.pathname === "/";
 
   useEffect(() => {
     setEntering(true);
@@ -17,7 +18,7 @@ export const Layout = () => {
     <div className="d-flex flex-column min-vh-100">
       <PageTransition />
       <Navbar />
-      <main className={`flex-grow-1 d-flex flex-column page-container ${entering ? "is-entering" : ""}`}>
+      <main className={`flex-grow-1 d-flex flex-column page-container ${isLanding ? "" : "favorites-bg"} ${entering ? "is-entering" : ""}`}>
         <Outlet /> {/*el router pinta la pagina actual */}
       </main>
       <Footer />
