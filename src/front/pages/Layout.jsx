b4361728370658554a0687a6f47d7b5f0f0bda7d
Edusanba <edu.santiagoba@hotmail.com>
2025-11-07 14:44:36 +0100

    fix: footer

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -14,13 +14,13 @@ export const Layout = () => {
     return () => clearTimeout(t);
   }, [location.pathname]);
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <PageTransition />
       <Navbar />
-      <main className={`flex-grow-1 page-container ${entering ? "is-entering" : ""}`}>
+      <main className={`flex-grow-1 d-flex flex-column page-container ${entering ? "is-entering" : ""}`}>
         <Outlet /> {/*el router pinta la pagina actual */}
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
