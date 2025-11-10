cfe7741f279871ce3b5d239ab9ce3248dde21d1c
Moiso10 <moisesrg05@gmail.com>
2025-11-10 20:17:42 +0000

    spinners listos

--- src/front/components/cloudinary.component.jsx
+++ src/front/components/cloudinary.component.jsx
@@ -2,61 +2,79 @@ import { useEffect, useState } from "react";
 import "./cloudinary.component.css";
 import cloudinaryServices from "../services/cloudinaryServices.jsx";
 
-function CloudinaryComponent({onUploadSuccess}) {
+function CloudinaryComponent({ onUploadSuccess,initialImage  }) {
     const [file, setFile] = useState(null);
-    const [url, setUrl] = useState("");
+    const [preview, setPreview] = useState(initialImage || "");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
     const [response, setResponse] = useState(null);
 
-   
 
-    useEffect(()=>{
-        const handleSubmit = async() =>{
-             setError("");
-        if (!file) return setError("Selecciona una imagen");
 
-        try {
-            setLoading(true);
-            const data = await cloudinaryServices.uploadImage(file);
-            setUrl(data.url);
-            setResponse(data);
-            if (onUploadSuccess) onUploadSuccess(data.url);
-        } catch (err) {
-            console.error(err);
-            setError("Error al subir la imagen: " + (err.message || err));
-        } finally {
-            setLoading(false);
+    useEffect(() => {
+        const handleSubmit = async () => {
+            setError("");
+            if (!file) return setError("Selecciona una imagen");
+
+            try {
+                setLoading(true);
+                const data = await cloudinaryServices.uploadImage(file);
+                setPreview(data.url);
+                setResponse(data);
+                if (onUploadSuccess) onUploadSuccess(data.url);
+            } catch (err) {
+                console.error(err);
+                setError("Error al subir la imagen: " + (err.message || err));
+                
+            } finally {
+                setLoading(false);
+            }
         }
-        }
-        
+
         if (file) {
             handleSubmit()
 
-            
+
         }
 
 
-    },[file])
+    }, [file])
 
     return (
         <div className="cloudinary-wrapper">
-            
-            
+            <label htmlFor="file-upload" className="avatar-container">
+        {loading ? (
+          <div className="uploading-overlay">
+            <div className="spinner"></div>
+            <p>Subiendo imagen...</p>
+          </div>
+        ) : (
+          <img
+            src={preview}
+            alt="Avatar"
+            className="avatar"
+          />
+        )}
+      </label>
+
+
+
+            <input
+                type="file"
+                accept="image/*"
+                className="upload-input"
+                onChange={(e) => {
+                    setFile(e.target.files[0]);
+                    setPreview("");
+                    setError("");
+
+                }}
+            />
            
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                        setFile(e.target.files[0]);
-                        setUrl("");
-                        setError("");
-                    }}
-                />
-            
+
 
             {error && <p className="cloudinary-error">{error}</p>}
-           
+
         </div>
     );
 }
