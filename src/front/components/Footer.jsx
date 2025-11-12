53117c40bb73544c20c14f0d1fef5f519e531f91
Edusanba <edu.santiagoba@hotmail.com>
2025-11-12 17:54:55 +0100

    Navbar y Footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,13 +1,12 @@
 import "./Footer.css";
+import logo from "../assets/img/logo1-01-02.png";
 
 
 export const Footer = () => (
-    <footer className="footer footer-3d mt-auto py-3 text-center">
-        <p>
-            Javier te odio
-        </p>
-        <p>
-            Hecho por Moises, Nohelia y Eduardo
-        </p>
+    <footer className="footer footer-3d mt-auto py-3">
+        <div className="footer-content">
+            <img src={logo} alt="MovieVerse logo" className="footer-logo" />
+            <p className="footer-text">Hecho por Moises, Nohelia y Eduardo</p>
+        </div>
     </footer>
 );
